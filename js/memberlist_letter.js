e1bcfec898c523060b40f47725ef8b43e3377644
Satoshi Morohara <g2116048@fun.ac.jp>
2016-08-23 13:13:16 +0900

    名前が間違っているファイルがあったので修正した #7

--- js/memberlist_letter.js
+++ js/memberlist_letter.js
@@ -7,25 +7,30 @@ function csv2Array(filePath){
 	var LF = String.fromCharCode(10);
 	var lines = data.responseText.split(LF);
 	for (var i = 0; i < lines.length;++i) {
+		console.log(lines[i]);
 		var cells = lines[i].split(",");
 		if( cells.length != 1 ) {
 			csvData.push(cells);
 		}
 	}
+
 	return csvData;
 }
 
-function myFunc(conditions){
+function listupMember(conditions){
 	var memberList = [];
 
 	var data = csv2Array("../resource/userlist.csv");
 	memberList = data;
 
+	console.log("myFunc");
+
 	return memberList;
 }
 
 window.onload = function(){
-	var limitYear = Date().getFullYear();;
+	var today = new Date();
+	var limitYear = today.getFullYear();;
 	var conditions={"SendFarResident": 0, "SendNurseryBrotherExist": 0, "limitYear": limitYear}
 	var members = listupMember(conditions);
 
