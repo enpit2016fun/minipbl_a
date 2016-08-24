0307a7634aaf8ce98f57568031502201edcca7af
Satoshi Morohara <g2116048@fun.ac.jp>
2016-08-24 16:29:04 +0900

    リスト画面（メール）のレイアウトと表示内容を修正した #41

--- js/mail_list.js
+++ js/mail_list.js
@@ -1,50 +1,113 @@
-var data1=[
-           ['卒園児の名前'  ,'親の名前'  ,'メールアドレス']
-           ,['田中一','田中五郎','example@mail.com' ]
-           ,['湯川秀樹','湯川秀俊','aaa@mail.com']
-           ,['山田奈緒子','山田花子','yamada@mail.com']
-           ,['森鴎外','森三郎','mori@mail.com']
-           ,['北海花子','北海一','hokkai@mail.com']
-           ];
+var targetuser_list = [];
 
-function $E(id_){ return document.getElementById(id_); }
-function hiTable(){}
-hiTable.set_td=function (tr,row,col_max){
-    for(var c=0;c<row.length;++c){
-        var td= document.createElement('td');
-        if(c>=col_max)td.style.display='none'; // <col>の数以上非表示
-        tr.appendChild(td);
-        td.innerHTML=row[c];
+function GetQueryString() {
+    if (1 < document.location.search.length) {
+        // 最初の1文字 (?記号) を除いた文字列を取得する
+        var query = document.location.search.substring(1);
+
+        // クエリの区切り記号 (&) で文字列を配列に分割する
+        var parameters = query.split('&');
+
+        var result = new Object();
+        for (var i = 0; i < parameters.length; i++) {
+            // パラメータ名とパラメータ値に分割する
+            var element = parameters[i].split('=');
+
+            var paramName = decodeURIComponent(element[0]);
+            var paramValue = decodeURIComponent(element[1]);
+
+            // パラメータ名をキーとして連想配列に追加する
+            result[paramName] = decodeURIComponent(paramValue);
+        }
+        return result;
     }
+    return null;
 }
-hiTable.set_th=function (tr,row,col_max){
-    for(var c=0;c<row.length;++c){
-        var td= document.createElement('th');
-        if(c>=col_max)td.style.display='none'; // <col>の数以上非表示
-        tr.appendChild(td);
-        td.innerHTML=row[c];
+
+function getData(path, conditions){
+    var csvData = new Array();
+    var data = new XMLHttpRequest();
+    data.open("GET", path, false);
+    data.send("");
+
+    var LF = String.fromCharCode(10);
+    var lines = data.responseText.split(LF);
+    for (var i = 0; i < lines.length; ++i) {
+        var cells = lines[i].split(",");
+        if(i == 0) continue;
+        if(parseInt(cells[1]) < conditions["limitYear"] ) continue;
+        if(cells[6] == "") continue;
+        if(typeof conditions["SendFarResident"] === "undefined" && cells[3] == "遠方") continue;
+        if(typeof conditions["SendNurseryBrotherExist"] === "undefined" && cells[6] == "有") continue;
+        csvData.push(cells);
     }
+
+    return csvData;
 }
-hiTable.set_table=function(tbl,param){
-    var thead=tbl.getElementsByTagName('thead')[0];
-    var tbody=tbl.getElementsByTagName('tbody')[0];
-    var tcols=tbl.getElementsByTagName('col');
-    var col_max=1000000;
-    if( tcols!=undefined && tcols.length!=0){// <col>がある場合数取得
-        col_max=tcols.length;
+
+function listupMember(conditions){
+    var data = getData("/resource/userlist.csv", conditions);
+
+    var tmp = data;
+    var motherlist = new Array();
+    for (var i = 0; i < tmp.length; ++i){
+        motherlist.push(tmp[i][4]);
     }
-    var d_start=0;
-    if( thead!=undefined ){ // <thead>がある場合先頭行セット
-        thead.innerHTML='';  // thead内容クリア
-        var tr= document.createElement('tr');
-        thead.appendChild(tr);
-        hiTable.set_th(tr,param[0],col_max);
-        d_start=1;
+    motherlist = motherlist.filter(function (x, i, self) {
+        return self.indexOf(x) === i;
+    });
+
+    memberList = new Array();
+    for(var i = 0; i < motherlist.length; ++i){
+        var namelist = new Array();
+        parent_name = "";
+        mail_address = "";
+        far = "";
+        brother = "";
+        for(var j = 0; j < tmp.length; ++j){
+            if(tmp[j][4] == motherlist[i]){
+                namelist.push(tmp[j][0]);
+                parent_name = tmp[j][4];
+                mail_address = tmp[j][5];
+                far = tmp[j][3];
+                brother = tmp[j][6];
+            }
+        }
+        memberList.push({"namelist":namelist, "parent_name":parent_name, "mail_address":mail_address, "far":far, "brother":brother});
     }
-    tbody.innerHTML=''; // tbody内容クリア
-    for(var n=d_start;n<param.length;++n){
-        var tr= document.createElement('tr');
-        tbody.appendChild(tr);
-        hiTable.set_td(tr,param[n],col_max);
-    }
+
+    return memberList;
 }
+
+window.onload = function(){
+    var today = new Date();
+    var limitYear = today.getFullYear()-6;
+    var param = GetQueryString();
+    var conditions={"SendFarResident": param['far'], "SendNurseryBrotherExist": param['brother'], "limitYear": limitYear}
+    var members = listupMember(conditions);
+
+    var table = document.getElementById('memberlist');
+    for (var i = 0; i < members.length; ++i){
+        name_contents = "";
+        for (var j = 0; j < members[i]["namelist"].length; ++j){
+            name_contents += memberList[i]["namelist"][j];
+            if(j != members[i]["namelist"].length - 1)
+                name_contents += "<br>";
+        }
+
+        var contents = "\
+            <tr>\
+                <td>#NAME#</td>\
+                <td>#PARENT_NAME#</td>\
+                <td>#MAIL_ADDRESS#</td>\
+                <td>#FAR#</td>\
+                <td>#BROTHER#</td>\
+            </tr>";
+        contents = contents.replace( /#NAME#/g , name_contents);
+        contents = contents.replace( /#PARENT_NAME#/g , members[i]["parent_name"]);
+        contents = contents.replace( /#MAIL_ADDRESS#/g , members[i]["mail_address"]);
+        contents = contents.replace( /#FAR#/g , members[i]["far"]);
+        contents = contents.replace( /#BROTHER#/g , members[i]["brother"]);
+        table.innerHTML += contents;
+    }
+};
